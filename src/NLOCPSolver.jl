955e141b750e8aed3d3eb57c5c30471539b10e6c
TimHalo86 <51740830+TimHalo86@users.noreply.github.com>
2024-06-21 10:50:47 -0400

    Add files via upload

--- src/NLOCPSolver.jl
+++ src/NLOCPSolver.jl
@@ -1,5 +1,36 @@
+push!(LOAD_PATH, ".")
 module NLOCPSolver
 
-# Write your package code here.
+using JuMP
+using Parameters
+using DataFrames
+using Ipopt
+
+include("types.jl")
+
+include("utils.jl")
+
+export  OptSolve!,
+        ExprIntegral,
+        RetrieveSolveStatus,
+        GetOptimizeValue!,
+        ResultsToDataFrame,
+        CreateEmptyFormulation,
+        DeleteElement,
+        WarmStart,
+        UpdateX0!
+
+include("setup.jl")
+
+export  defineOCP,
+        defineStates!,
+        defineControls!,
+        defineTolerance!,
+        ValidateScheme,
+        ConfigurePredefined,
+        CheckOCPFormulation,
+        defineSolver!,
+        CalXvar,
+        OCPdef!
 
 end
